0f3ea53fb81a54c131ec09adc0f2563054872b96
ojsung <oehyun.sung@vivintinternet.com>
2019-12-20 19:03:34 -0700

    added an index.d.ts for typescript

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export as namespace extendStringLib
+export function extendString(
+  stringOrStringArray: string | string[],
+  stringToExtend?: string,
+  delimiter?: string,
+  useJoin?: boolean
+): string
